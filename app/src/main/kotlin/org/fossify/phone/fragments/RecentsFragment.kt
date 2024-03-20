FossifyOrg/Phone

commit a36e47b3f5fb3071cdb067266626aa2a80e5ff81

Removed call history limit (#125)

app/src/main/kotlin/org/fossify/phone/fragments/RecentsFragment.kt
@@ -61,8 +61,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
     override fun refreshItems(callback: (() -> Unit)?) {
         val privateCursor = context?.getMyContactsCursor(false, true)
         val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
-        val querySize = allRecentCalls.size.coerceAtLeast(MIN_RECENTS_THRESHOLD)
-        RecentsHelper(context).getRecentCalls(groupSubsequentCalls, querySize) { recents ->
+        RecentsHelper(context).getRecentCalls(groupSubsequentCalls, MIN_RECENTS_THRESHOLD, allRecentCalls) { recents ->
             ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
                 val privateContacts = MyContactsContentProvider.getContacts(context, privateCursor)
 
@@ -126,8 +125,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
     private fun getMoreRecentCalls() {
         val privateCursor = context?.getMyContactsCursor(false, true)
         val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
-        val querySize = allRecentCalls.size.plus(MIN_RECENTS_THRESHOLD)
-        RecentsHelper(context).getRecentCalls(groupSubsequentCalls, querySize) { recents ->
+        RecentsHelper(context).getRecentCalls(groupSubsequentCalls, MIN_RECENTS_THRESHOLD, allRecentCalls) { recents ->
             ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
                 val privateContacts = MyContactsContentProvider.getContacts(context, privateCursor)
 

app/src/main/kotlin/org/fossify/phone/helpers/RecentsHelper.kt
@@ -17,7 +17,7 @@ class RecentsHelper(private val context: Context) {
     private val QUERY_LIMIT = 200
     private val contentUri = Calls.CONTENT_URI
 
-    fun getRecentCalls(groupSubsequentCalls: Boolean, maxSize: Int = QUERY_LIMIT, callback: (List<RecentCall>) -> Unit) {
+    fun getRecentCalls(groupSubsequentCalls: Boolean, maxSize: Int = QUERY_LIMIT, previousRecents: List<RecentCall> = ArrayList(), callback: (List<RecentCall>) -> Unit) {
         val privateCursor = context.getMyContactsCursor(false, true)
         ensureBackgroundThread {
             if (!context.hasPermission(PERMISSION_READ_CALL_LOG)) {
@@ -31,13 +31,13 @@ class RecentsHelper(private val context: Context) {
                     contacts.addAll(privateContacts)
                 }
 
-                getRecents(contacts, groupSubsequentCalls, maxSize, callback = callback)
+                getRecents(contacts, groupSubsequentCalls, maxSize, previousRecents, callback = callback)
             }
         }
     }
 
     @SuppressLint("NewApi")
-    private fun getRecents(contacts: List<Contact>, groupSubsequentCalls: Boolean, maxSize: Int, callback: (List<RecentCall>) -> Unit) {
+    private fun getRecents(contacts: List<Contact>, groupSubsequentCalls: Boolean, maxSize: Int, previousRecents: List<RecentCall>, callback: (List<RecentCall>) -> Unit) {
 
         val recentCalls = mutableListOf<RecentCall>()
         var previousRecentCallFrom = ""
@@ -61,16 +61,25 @@ class RecentsHelper(private val context: Context) {
             accountIdToSimIDMap[it.handle.id] = it.id
         }
 
+        var selection: String? = null
+        var selectionParams: Array<String>? = null
+
+        val lastId = previousRecents.lastOrNull()?.id
+        if (lastId != null) {
+            selection = "${Calls._ID} < ?"
+            selectionParams = arrayOf(lastId.toString())
+        }
+
         val cursor = if (isNougatPlus()) {
             // https://issuetracker.google.com/issues/175198972?pli=1#comment6
             val limitedUri = contentUri.buildUpon()
                 .appendQueryParameter(Calls.LIMIT_PARAM_KEY, QUERY_LIMIT.toString())
                 .build()
             val sortOrder = "${Calls.DATE} DESC"
-            context.contentResolver.query(limitedUri, projection, null, null, sortOrder)
+            context.contentResolver.query(limitedUri, projection, selection, selectionParams, sortOrder)
         } else {
             val sortOrder = "${Calls.DATE} DESC LIMIT $QUERY_LIMIT"
-            context.contentResolver.query(contentUri, projection, null, null, sortOrder)
+            context.contentResolver.query(contentUri, projection, selection, selectionParams, sortOrder)
         }
 
         val contactsWithMultipleNumbers = contacts.filter { it.phoneNumbers.size > 1 }
@@ -196,7 +205,7 @@ class RecentsHelper(private val context: Context) {
         val recentResult = recentCalls
             .filter { !context.isNumberBlocked(it.phoneNumber, blockedNumbers) }
 
-        callback(recentResult)
+        callback(previousRecents.plus(recentResult))
     }
 
     fun removeRecentCalls(ids: List<Int>, callback: () -> Unit) {

commit dd10748147dc606095f657de7b895b72aa24f419

Fix deleting call history items
The problem was that the recent call items were no longer being deleted from the cached list as a result of lazy loading. This was a UI glitch (introduced recently in #125) and it did not affect the actual deletion from the database.

app/src/main/kotlin/org/fossify/phone/fragments/RecentsFragment.kt
@@ -23,7 +23,7 @@ import org.fossify.phone.models.RecentCall
 class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment<MyViewPagerFragment.RecentsInnerBinding>(context, attributeSet),
     RefreshItemsListener {
     private lateinit var binding: FragmentRecentsBinding
-    private var allRecentCalls = listOf<RecentCall>()
+    private var allRecentCalls = mutableListOf<RecentCall>()
     private var recentsAdapter: RecentCallsAdapter? = null
 
     override fun onFinishInflate() {
@@ -65,7 +65,8 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
             ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
                 val privateContacts = MyContactsContentProvider.getContacts(context, privateCursor)
 
-                allRecentCalls = recents
+                allRecentCalls.clear()
+                allRecentCalls += recents
                     .setNamesIfEmpty(contacts, privateContacts)
                     .hidePrivateContacts(privateContacts, SMT_PRIVATE in context.baseConfig.ignoredContactSources)
 
@@ -77,7 +78,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
         }
     }
 
-    private fun gotRecents(recents: List<RecentCall>) {
+    private fun gotRecents(recents: MutableList<RecentCall>) {
         if (recents.isEmpty()) {
             binding.apply {
                 showOrHidePlaceholder(true)
@@ -92,7 +93,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
             }
 
             if (binding.recentsList.adapter == null) {
-                recentsAdapter = RecentCallsAdapter(activity as SimpleActivity, recents.toMutableList(), binding.recentsList, this, true) {
+                recentsAdapter = RecentCallsAdapter(activity as SimpleActivity, recents, binding.recentsList, this, true) {
                     val recentCall = it as RecentCall
                     if (context.config.showCallConfirmation) {
                         CallConfirmationDialog(activity as SimpleActivity, recentCall.name) {
@@ -130,7 +131,8 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
             ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
                 val privateContacts = MyContactsContentProvider.getContacts(context, privateCursor)
 
-                allRecentCalls = recents
+                allRecentCalls.clear()
+                allRecentCalls += recents
                     .setNamesIfEmpty(contacts, privateContacts)
                     .hidePrivateContacts(privateContacts, SMT_PRIVATE in context.baseConfig.ignoredContactSources)
 
@@ -150,7 +152,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
                 val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
                 RecentsHelper(context).getRecentCalls(groupSubsequentCalls) { recents ->
                     activity?.runOnUiThread {
-                        gotRecents(recents)
+                        gotRecents(recents.toMutableList())
                     }
                 }
             }
dd10748147dc606095f657de7b895b72aa24f419
Naveen <snaveen935@gmail.com>
2024-03-21 02:43:45 +0530

    Fix deleting call history items
    The problem was that the recent call items were no longer being deleted from the cached list as a result of lazy loading. This was a UI glitch (introduced recently in #125) and it did not affect the actual deletion from the database.

--- app/src/main/kotlin/org/fossify/phone/fragments/RecentsFragment.kt
+++ app/src/main/kotlin/org/fossify/phone/fragments/RecentsFragment.kt
@@ -23,7 +23,7 @@ import org.fossify.phone.models.RecentCall
 class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment<MyViewPagerFragment.RecentsInnerBinding>(context, attributeSet),
     RefreshItemsListener {
     private lateinit var binding: FragmentRecentsBinding
-    private var allRecentCalls = listOf<RecentCall>()
+    private var allRecentCalls = mutableListOf<RecentCall>()
     private var recentsAdapter: RecentCallsAdapter? = null
 
     override fun onFinishInflate() {
@@ -65,7 +65,8 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
             ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
                 val privateContacts = MyContactsContentProvider.getContacts(context, privateCursor)
 
-                allRecentCalls = recents
+                allRecentCalls.clear()
+                allRecentCalls += recents
                     .setNamesIfEmpty(contacts, privateContacts)
                     .hidePrivateContacts(privateContacts, SMT_PRIVATE in context.baseConfig.ignoredContactSources)
 
@@ -77,7 +78,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
         }
     }
 
-    private fun gotRecents(recents: List<RecentCall>) {
+    private fun gotRecents(recents: MutableList<RecentCall>) {
         if (recents.isEmpty()) {
             binding.apply {
                 showOrHidePlaceholder(true)
@@ -92,7 +93,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
             }
 
             if (binding.recentsList.adapter == null) {
-                recentsAdapter = RecentCallsAdapter(activity as SimpleActivity, recents.toMutableList(), binding.recentsList, this, true) {
+                recentsAdapter = RecentCallsAdapter(activity as SimpleActivity, recents, binding.recentsList, this, true) {
                     val recentCall = it as RecentCall
                     if (context.config.showCallConfirmation) {
                         CallConfirmationDialog(activity as SimpleActivity, recentCall.name) {
@@ -130,7 +131,8 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
             ContactsHelper(context).getContacts(showOnlyContactsWithNumbers = true) { contacts ->
                 val privateContacts = MyContactsContentProvider.getContacts(context, privateCursor)
 
-                allRecentCalls = recents
+                allRecentCalls.clear()
+                allRecentCalls += recents
                     .setNamesIfEmpty(contacts, privateContacts)
                     .hidePrivateContacts(privateContacts, SMT_PRIVATE in context.baseConfig.ignoredContactSources)
 
@@ -150,7 +152,7 @@ class RecentsFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
                 val groupSubsequentCalls = context?.config?.groupSubsequentCalls ?: false
                 RecentsHelper(context).getRecentCalls(groupSubsequentCalls) { recents ->
                     activity?.runOnUiThread {
-                        gotRecents(recents)
+                        gotRecents(recents.toMutableList())
                     }
                 }
             }
