efb9bbeb96c1cb5e61c0b906f614cd4217b6bbdf
Naveen Singh <snaveen935@gmail.com>
2025-06-05 15:56:42 +0530

    style: put parameters on separate lines

--- app/src/main/kotlin/org/fossify/phone/models/SIMAccount.kt
+++ app/src/main/kotlin/org/fossify/phone/models/SIMAccount.kt
@@ -2,4 +2,10 @@ package org.fossify.phone.models
 
 import android.telecom.PhoneAccountHandle
 
-data class SIMAccount(val id: Int, val handle: PhoneAccountHandle, val label: String, val phoneNumber: String, val color: Int)
+data class SIMAccount(
+    val id: Int,
+    val handle: PhoneAccountHandle,
+    val label: String,
+    val phoneNumber: String,
+    val color: Int,
+)
